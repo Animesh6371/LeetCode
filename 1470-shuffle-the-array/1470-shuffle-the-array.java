f8f0c75696c5fde6f7d22e59402caa09806f997b
Animesh Dash <dashanimesh6371@gmail.com>
2026-04-30 10:41:51 +0530

    Time: 1 ms (12.99%), Space: 46.4 MB (62.56%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,10 +1,18 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
         int[] ans= new int[nums.length];
-        for(int i=0;i<n;i++)
+        int x=0;
+        int y=n;
+        for(int i=0;i<2*n;i++)
         {
-            ans[2*i]=nums[i];
-            ans[2*i+1] = nums[i+n];
+            if(i%2 == 0){
+                ans[i]=nums[x];
+                x++;
+            }
+            else {
+                ans[i]=nums[y];
+                y++;
+            }
         }
         return ans;
     }
